fa3c88472a5beedc7851fc26af7a0d558c30a733
dhawansolanki <dhawansolanki2103@gmail.com>
2022-12-20 18:45:02 +0530

    [GTM]

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
 import { useContext } from "react";
 import { themeContext } from "./Context";
+import ReactGA from 'react-ga';
 function App() {
   const theme = useContext(themeContext);
   const darkMode = theme.state.darkMode;
